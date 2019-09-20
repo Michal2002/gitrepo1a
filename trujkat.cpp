70e5837d1b0c064e90ffbcb4809e8e22783b7657
MARIAMJESTEM <mariamazur.2000@wp.pl>
2019-09-20 09:42:26 +0200

    botstrap3

---
+++ trujkat.cpp
@@ -0,0 +1,54 @@
+/*
+ * trujkat.cpp
+ * 
+ * Copyright 2019  <>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+int liczba1;
+int liczba2;
+int liczba3;
+
+int main(int argc, char **argv){
+{ 
+    cout<<"podaj pierwszy bok:"; 
+    cin>>liczba1;
+    cout<<"podaj drugi bok:"; 
+    cin>>liczba2;
+    cout<<"podaj trzeci bok:"; 
+    cin>>liczba3;
+    if(liczba1+liczba2 >liczba3){
+        if(liczba2+liczba3 >liczba1){
+            if(liczba1+liczba3 >liczba2){
+                cout<<"dany trójkąt istnieje";
+            else(cout<<"dany trójkąt nieistniejw");
+    
+    
+    }
+    }
+    }
+
+}
+{
+return 0;
+}
+
