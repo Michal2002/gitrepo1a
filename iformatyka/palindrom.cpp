9b916e978e51bf0d3bcd644a183148fd088ee32f
Michal2002 <mariamazur.2000@wp.pl>
2019-11-18 10:35:04 +0100

    euklidess

--- iformatyka/palindrom.cpp
+++ iformatyka/palindrom.cpp
@@ -26,45 +26,65 @@
 #include <cstring>
 using namespace std;
 
-bool czypalindrom(char tab[])
-{
+bool czy_palindrom(char tab[]) {
     int rozmiar = strlen(tab);
-    for(int i=0 ;i < rozmiar / 2 ;i++)
-    {
-        if (tab[i]==tab[rozmiar - i - 1])
-            continue;
+    int i = 0;
+    for(i = 0; i < rozmiar / 2; i++) {
+        if(tab[i] == tab[rozmiar - 1 - i])
+            ;
         else
             return false;
-        
-         
     }
     return true;
-} 
-void removespace(char tab1[])
-{
-    int rozmiar = strlen(tab);
-    for(int i=0 ;i < rozmiar / 2 ;i++)
-    if(tab1[i] != ' ')
-    
-    
-} 
+}
+
+void removeSpaces(char tab1[], char tab2[]) {
+    int rozmiar = strlen(tab1);
+    int i = 0;
+    for(i = 0; i < rozmiar; i++) {
+        if (tab1[i] != ' '){
+            tab2[i] = tab1[i];
+        }
+    }
+    tab2[i] = '\0';
+}
+bool czy_palindrom2(char tab[]){
+	int rozmiar = strlen(tab);
+	for(int i = 0; i < rozmiar/2; i++){
+		if(tab[i] == ' ') i++;
+		if(tab[i] == tab[rozmiar- 1 - i])
+			;
+		else
+			return false;
+			}
+
+		return true;
+	}
+
 int main(int argc, char **argv)
 {
-    int r =20;
+    // n! = 1 * ... * n
+    // n! = (n-1)! * n
+    int r = 20;
     char napis1[r];
     char napis2[r];
-    cin.getline(napis, 20);
-    removespace(napis1, napis2);
-    if(czypalindrom(napis))
+    cin.getline(napis1, 20);
+    removeSpaces(napis1, napis2);
+
+    cout << napis2 << endl;
+    cout << strlen(napis2) << endl;
+
+    if (czy_palindrom(napis2)) 
     {
-        cout<<"========================================================"
-        cout<<"to palindrom!"<<endl;
+        cout << "==================================================="<<endl;
+        cout << " to palindrom " << endl;
+    } else 
+    {
+        cout << "==================================================="<<endl;
+        cout << " to nie palindrom " << endl;
     }
-    else
-        cout<<"========================================================"
-        cout<<"to nie palindrom"<<endl;
-    cout<<napis;
-	
+
+
 	return 0;
 }
 
